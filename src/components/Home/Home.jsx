cdc2c6e7b552b489128d3741375bf15576121a86
omar magdy <omarmagdyzz12@gmail.com>
2025-01-21 16:42:37 +0200

    fix images deployment issue

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
       <div className="rounded-lg bg-slate-950 w-3/4 md:w-1/2 lg:w-1/3">
         <img
           className="w-full h-auto rounded-lg"
-          src="public/myPhoto.png"
+          src="/public/myPhoto.png"
           alt="Omar Magdy"
         />
       </div>
