fd103b74f5fd710802f5ad39d0193c8868a74629
omar magdy <omarmagdyzz12@gmail.com>
2025-01-21 16:15:49 +0200

    trying to fix img issue

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,93 +1,3 @@
-// import React, { useState } from "react";
-// import { Link, NavLink } from "react-router-dom";
-
-// const Header = () => {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <nav className="p-4 bg-gray-100 shadow-md flex items-center justify-between">
-//       {/* Logo */}
-//       <div className="flex items-center">
-//         <Link to="/">
-//           <img
-//             className="w-14 h-11 rounded-xl"
-//             src="src/assets/Omar-Magdy.svg"
-//             alt="logo"
-//           />
-//         </Link>
-//       </div>
-//       {/* Navigation Links */}
-//       <ul className="flex  md:flex-row md:space-x-6 p-12 md:p-0 ">
-//         <li>
-//           <NavLink
-//             to="/"
-//             className={({ isActive }) =>
-//               isActive
-//                 ? "bg-black text-white px-4 py-2 rounded-lg"
-//                 : "text-black hover:text-gray-700 px-4 py-2"
-//             }
-//           >
-//             Home
-//           </NavLink>
-//         </li>
-//         <li>
-//           <NavLink
-//             to="about"
-//             className={({ isActive }) =>
-//               isActive
-//                 ? "bg-black text-white px-4 py-2 rounded-lg"
-//                 : "text-black hover:text-gray-700 px-4 py-2"
-//             }
-//           >
-//             About Me
-//           </NavLink>
-//         </li>
-//         <li>
-//           <NavLink
-//             to="skills"
-//             className={({ isActive }) =>
-//               isActive
-//                 ? "bg-black text-white px-4 py-2 rounded-lg"
-//                 : "text-black hover:text-gray-700 px-4 py-2"
-//             }
-//           >
-//             Skills
-//           </NavLink>
-//         </li>
-//         <li>
-//           <NavLink
-//             to="projects"
-//             className={({ isActive }) =>
-//               isActive
-//                 ? "bg-black text-white px-4 py-2 rounded-lg"
-//                 : "text-black hover:text-gray-700 px-4 py-2"
-//             }
-//           >
-//             Projects
-//           </NavLink>
-//         </li>
-//         <li>
-//           <NavLink
-//             to="contact"
-//             className={({ isActive }) =>
-//               isActive
-//                 ? "bg-black text-white px-4 py-2 rounded-lg"
-//                 : "text-black hover:text-gray-700 px-4 py-2"
-//             }
-//           >
-//             Contact Me
-//           </NavLink>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// export default Header;
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
@@ -105,7 +15,7 @@ const Header = () => {
         <Link to="/">
           <img
             className="w-14 h-11 rounded-xl"
-            src="src/assets/Omar-Magdy.svg"
+            src="src/images/Omar-Magdy.svg"
             alt="logo"
           />
         </Link>
