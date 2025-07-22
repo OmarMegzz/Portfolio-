ee30b7ed79a450fae8553d9060e21b1f93651146
omar magdy <omarmagdyzz12@gmail.com>
2025-07-22 16:48:20 +0300

    add animation to Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,17 +3,41 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
 import { Link } from "react-router-dom";
+import { Typewriter } from "react-simple-typewriter";
+// import TextLoop from "react-text-loop";
 
 const Home = () => {
   return (
-    <div className="h-full p-8 flex flex-col md:flex-row items-center justify-evenly gap-8">
-      {/* Text Section */}
+    <div className="h-full  p-8 flex flex-col md:flex-row items-center justify-around gap-8">
       <div className="text-center md:text-left flex flex-col items-center md:items-start">
         <div className="info-text">
+          <h4 className="text-xl md:text-2xl font-mono text-gray-500 min-h-[2.5rem] w-[280px] md:w-[360px] overflow-hidden">
+            <Typewriter
+              words={["Welcome to My Portfolio"]}
+              loop={true}
+              typeSpeed={100}
+              deleteSpeed={80}
+              delaySpeed={1000}
+            />
+          </h4>
           <h2 className="text-3xl  font-bold">I'M</h2>
-          <h1 className="text-5xl md:text-7xl font-bold">Omar Magdy</h1>
-          <h3 className="text-xl md:text-2xl font-extrabold text-gray">
+          <h1 className="text-3xl md:text-4xl font-bold">Omar Magdy</h1>
+          <h3 className="text-xl md:text-2xl font-extrabold text-gray ">
             Front-end Developer
+            {/* <Typewriter
+              words={[
+                "Front-end Developer",
+                "React Developer",
+                "Chasing Better Every Day",
+                "Built Real Projects with React",
+              ]}
+              loop={true}
+              cursor
+              cursorStyle="_"
+              typeSpeed={100}
+              deleteSpeed={80}
+              delaySpeed={1000}
+            /> */}
           </h3>
         </div>
         <div className="flex items-center justify-center md:justify-start gap-4 pt-6">
@@ -29,13 +53,12 @@ const Home = () => {
           <Link to={"https://github.com/OmarMegzz"} target={"_blank"}>
             <FontAwesomeIcon
               icon={faGithub}
-              className="text-gray-500 hover:text-black w-8 h-8 transition-all"
+              className="text-gray-500 hover:text-green-500  w-8 h-8 transition-all"
             />
           </Link>
         </div>
       </div>
 
-      {/* Image Section */}
       <div className="rounded-lg bg-slate-950 w-3/4 md:w-1/2 lg:w-1/3">
         <img
           className="w-full h-auto rounded-lg"
